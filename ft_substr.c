5a9f47e6a9feff29aeede455d983d6ed1eea5342
Milad <milad.rahmat@gmail.com>
2024-04-27 12:33:40 +0300

    Created more error checks and added null terminator to ft_substring

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: mrahmat- < mrahmat-@student.hive.fi >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 12:11:14 by mrahmat-          #+#    #+#             */
-/*   Updated: 2024/04/22 12:26:15 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2024/04/27 11:54:56 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*res;
 	unsigned int	i;
 
+	if (start > ft_strlen((char *)s))
+		return (0);
 	i = 0;
 	res = (char *)malloc(len * sizeof(char));
-	if (res == 0)
-		return (0);
+	if (res == NULL)
+		return (NULL);
 	while (i < len)
 	{
 		res[i] = s[start];
 		i++;
 		start++;
 	}
+	res[i] = '\0';
 	return (res);
 }
 /* 
