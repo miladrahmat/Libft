cefec350ebb002827ab3f5ebeb4db438dd8436b7
Milad <milad.rahmat@gmail.com>
2024-04-29 11:41:11 +0300

    Created ft_itoa (converts integers into characters, allocates and copies them to a string) and added to header file

---
+++ ft_itoa.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrahmat- < mrahmat-@student.hive.fi >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 09:24:38 by mrahmat-          #+#    #+#             */
+/*   Updated: 2024/04/29 11:37:30 by mrahmat-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	check_size(int n)
+{
+	int	check;
+	int	temp;
+	int	count;
+
+	temp = n;
+	check = 0;
+	count = 0;
+	if (temp == -2147483648)
+	{
+		count = 11;
+		return (count);
+	}
+	if (temp < 0)
+	{
+		temp *= -1;
+		count++;
+	}
+	while (temp >= 10)
+	{
+		check += temp % 10;
+		temp = temp / 10;
+		check = check * 10;
+		count++;
+	}
+	return (count + 1);
+}
+
+static char	*converter(int n, int size, char *string)
+{
+	unsigned int	i;
+
+	if (n == -2147483648)
+	{
+		string[0] = '-';
+		string[1] = '2';
+		n = 147483648;
+	}
+	string[size] = '\0';
+	i = size - 1;
+	if (n < 0)
+	{
+		string[0] = '-';
+		n *= -1;
+	}
+	while (n >= 10)
+	{
+		string[i--] = ((n % 10) + '0');
+		n = n / 10;
+	}
+	if (n < 10)
+		string[i] = (n + '0');
+	return (string);
+}
+
+char	*ft_itoa(int n)
+{
+	unsigned int		size;
+	char				*res;
+
+	size = check_size(n);
+	res = (char *)malloc(size * sizeof(char) + 1);
+	if (res == NULL)
+		return (NULL);
+	res = converter(n, size, res);
+	return (res);
+}
+/* 
+#include <stdio.h>
+int	main()
+{
+	int	nbr = -1111111111;
+	char *res;
+
+	res = ft_itoa(nbr);
+	printf("Integer %d converted to %s\n", nbr, res);
+	free(res);
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mrahmat- < mrahmat-@student.hive.fi >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:12:19 by mrahmat-          #+#    #+#             */
-/*   Updated: 2024/04/29 09:19:50 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2024/04/29 11:38:26 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
