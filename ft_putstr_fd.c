b6f5934e637e755d0dd885ea7b3a97a83032a523
Milad Rahmat Abadi <mrahmat-@student.hive.fi>
2024-04-30 11:40:40 +0300

    Created ft_putstr_fd (Outputs the given string to given file descriptor) and added to header file.

---
+++ ft_putstr_fd.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 11:35:25 by mrahmat-          #+#    #+#             */
+/*   Updated: 2024/04/30 11:38:32 by mrahmat-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+/* 
+int	main()
+{
+	char *str = "Hello World!";
+
+	ft_putstr_fd(str, 1);
+	return (0);
+} */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:12:19 by mrahmat-          #+#    #+#             */
-/*   Updated: 2024/04/30 11:33:22 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2024/04/30 11:39:16 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char*));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
