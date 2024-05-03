dc48566c221342330c24d38c514269e1f774d48b
Milad Rahmat Abadi <mrahmat-@student.hive.fi>
2024-05-03 14:59:38 +0300

    Created functions ft_lstnew, ft_lstadd_front, ft_lstsize and ft_lstlast. Added to header file and updated Makefile to include bonuses

--- Makefile
+++ Makefile
@@ -6,7 +6,10 @@ SOURCES = \
 		ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c ft_split.c ft_strchr.c \
 		ft_strdup.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlcpy.c ft_strlen.c ft_strmapi.c \
 		ft_strncmp.c ft_strnstr.c ft_strrchr.c ft_strtrim.c ft_substr.c ft_tolower.c ft_toupper.c
+BSOURCES = \
+		ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c ft_lstlast_bonus.c
 OBJECTS = $(SOURCES:.c=.o)
+BOBJECTS = $(BSOURCES:.c=.o)
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 
@@ -15,12 +18,14 @@ all: $(NAME)
 #The $? means the names of all the files? (if I understood correctly)
 $(NAME): $(OBJECTS)
 		$(AR) -r $@ $?
+bonus: $(OBJECTS) $(BOBJECTS)
+	$(AR) -r $(NAME) $(OBJECTS) $(BOBJECTS)
 %.o: %.c
 		$(CC) -c $(CFLAGS) $?
 clean:
-		rm -f $(OBJECTS)
+		rm -f $(OBJECTS) $(BOBJECTS)
 fclean: clean
 		rm -f $(NAME)
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all clean fclean re bonus
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:12:19 by mrahmat-          #+#    #+#             */
-/*   Updated: 2024/05/02 18:21:37 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2024/05/03 14:31:14 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,16 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
