1034294bed588c2049be303f6af7fe926838e6bb
Milad Rahmat Abadi <mrahmat-@student.hive.fi>
2024-05-06 17:40:55 +0300

    Added a error check to see that the given function is not NULL

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 12:23:03 by mrahmat-          #+#    #+#             */
-/*   Updated: 2024/05/06 12:40:45 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2024/05/06 17:40:05 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (lst == NULL)
+	if (lst == NULL || del == NULL)
 		return ;
 	del(lst->content);
 	free(lst);
