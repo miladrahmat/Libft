fc3e8310b88a589c8ffd81de40ebce3f488a3c76
Milad Rahmat Abadi <mrahmat-@student.hive.fi>
2024-05-06 12:44:22 +0300

    Created ft_lstdelone function (deletes the content of the node the list is pointing to using an external function given as a parameter and frees the node), added to header file and added to Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:12:19 by mrahmat-          #+#    #+#             */
-/*   Updated: 2024/05/06 12:05:46 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2024/05/06 12:28:31 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
